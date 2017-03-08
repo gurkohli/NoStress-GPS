bc313d30cf71934afe342b193e88f83ea83756b7
Gur Kohli <gkohli@sfu.ca>
2017-03-08 12:15:52 -0800

    Did the following:
    1) Added libBleManager.a library to iOS project.
    2) Added a sample code to get started with ble-manager.
    3) Changed bundle name of the app from react-native-example to NoStress-GPS-Mobile
    
    Tested the app on my iPhone and bluetooth devices are being discovered.
    
    Next Steps:
    1) Start working on RasPi to emit bluetooth and add logic to the app to pick it and connect.

--- app/index.ios.js
+++ app/index.ios.js
@@ -8,24 +8,61 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TouchableHighlight,
+  NativeAppEventEmitter,
   Text,
   View
 } from 'react-native';
+import BleManager from 'react-native-ble-manager';
 
 export default class app extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      ble: null,
+      scanning: false
+    }
+  }
+
+  componentDidMount() {
+    BleManager.start({showAlert: false})
+    this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
+
+    NativeAppEventEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral); 
+  }
+
+  handleScan() {
+    BleManager.scan([], 30, true).then((results) => {
+      console.log("Scanning", results); 
+    });
+  }
+
+  toggleScanning(bool) {
+    if (bool) {
+      this.setState({scanning:true})
+      this.scanning = setInterval(() => this.handleScan(), 3000);
+    } else {
+      this.setState({scanning:false, ble: null})
+      clearInterval(this.scanning);
+    }
+  }
+
+  handleDiscoverPeripheral(data) {
+    console.log("Got ble data", data);
+    this.setState({ble: data})
+  }
   render() {
+    const bleList = this.state.ble 
+                ? <Text> Device found : {this.state.ble.name} </Text>
+                : <Text> No Devices Nearby </Text>
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableHighlight style={{padding:20, backgroundColor:'#ccc'}} onPress={() => this.toggleScanning(!this.state.scanning) }>
+            <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
+        </TouchableHighlight>
+
+        {bleList}
       </View>
     );
   }
