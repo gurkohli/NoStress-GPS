30e0387fe185ba072ecdbe81e5391ef43ba56750
Gur Kohli <gkohli@sfu.ca>
2017-05-17 22:58:39 -0700

    Merged testing branch to master. Moving towards mapbox-gl

--- module/interface/public/main.js
+++ module/interface/public/main.js
@@ -1,6 +1,18 @@
-var RASPI_HOST = 'http://localhost:8080'
-var tileURL = RASPI_HOST + '/styles/nostress/rendered/{z}/{x}/{y}.png'
-var tileLayer = new L.TileLayer(tileURL, {minZoom: 10, maxZoom: 20});
+// var RASPI_HOST = 'http://localhost:8080'
+// var tileURL = RASPI_HOST + '/styles/nostress/rendered/{z}/{x}/{y}.png'
+// var tileLayer = new L.TileLayer(tileURL, {minZoom: 10, maxZoom: 20});
+
+// const ZOOM_LEVEL_SLOW = 18;
+// const ZOOM_LEVEL_MED = 17;
+// const ZOOM_LEVEL_FAST = 16;
+//
+// var zoomLevelGlobal = ZOOM_LEVEL_SLOW; // variable to update current zoom level on next map render
+//
+// var latlngs = [ // temp data for simulations line
+//     [49.2811, -123.10],
+//     [49.2811, -123.00]
+// ];
+// var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
 
 // var map = L.map('map')
 //
@@ -126,16 +138,45 @@ map.on('load', function () {
     });
 });
 
-//socket.on('routing', function(data) {
-	// if (data.geometry) {
-	// 	var geoJSONFeature = {
-	// 		"type": "Feature",
-	// 		"geometry": data.geometry
-	// 	}
-	// 	if (!isFirstData) {
-	// 		routingLayer.clearLayers()
-	// 	}
-	// 	isFirstData = false
-	// 	routingLayer.addData(geoJSONFeature)
-	// }
-//});
+
+// // BLE handle
+// socket.on('routing', function(data) {
+// 	if (data.geometry) {
+// 		var geoJSONFeature = {
+// 			"type": "Feature",
+// 			"geometry": data.geometry
+// 		}
+// 		if (!isFirstData) {
+// 			routingLayer.clearLayers()
+// 		}
+// 		isFirstData = false
+// 		routingLayer.addData(geoJSONFeature)
+// 	}
+// });
+
+// // GPS sensor handle
+// var counter = 10;
+// socket.on('gps', function(data) {
+// 	markers.clearLayers(); // clear previous marker
+// 	//console.log("gps data");
+// 	L.marker([data[0], data[1]]).addTo(markers); // use default leaflet marker, todo: change to dot or smth like  that
+// 	counter--;
+// 	if(counter==0) // update the View of the map not everytime but periodically
+// 	{
+// 		map.setView([data[0], data[1]], zoomLevelGlobal); // updates view and centers your position
+// 		counter=10;
+// 	}
+//
+// });
+
+// socket.on('acc', function(data) {
+// 	if(data == "Fast")
+// 	{
+// 		zoomLevelGlobal = ZOOM_LEVEL_FAST;
+// 	}
+// 	else
+// 	{
+// 		zoomLevelGlobal = ZOOM_LEVEL_SLOW;
+// 	}
+//
+// });
